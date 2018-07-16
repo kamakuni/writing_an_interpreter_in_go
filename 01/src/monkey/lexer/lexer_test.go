5210e9e73496d515cab2575e365402f4ac9fd72e
kamakuni <superradio86@gmail.com>
2018-07-16 10:09:05 +0900

    add tests for lexer

---
+++ 01/src/monkey/lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer
+
+import (
+	"github.com/kamakuni/writing_an_interpreter_in_go/01/src/monkey/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := "=+(){},;"
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPARAN, "("},
+		{token.RPARAN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", l, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", l, tt.expectedLiteral, tok.Literal)
+		}
+	}
+
+}
