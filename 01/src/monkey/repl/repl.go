014cab2b2b3c5f944555bd9470e0b6497c7757e6
kamakuni <superradio86@gmail.com>
2018-07-20 00:50:55 +0900

    fix Start function

--- 01/src/monkey/repl/repl.go
+++ 01/src/monkey/repl/repl.go
@@ -12,14 +12,16 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	fmt.Printf(PROMPT)
-	scanned := scanner.Scan()
-	if !scanned {
-		return
-	}
-	line := scanner.Text()
-	l := lexer.New(line)
-	for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		fmt.Printf("%+v\n", tok)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
 	}
 }
