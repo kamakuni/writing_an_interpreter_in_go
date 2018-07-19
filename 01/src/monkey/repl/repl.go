6e94184af13dcddc4750867e2842e09ca04fa9f9
kamakuni <superradio86@gmail.com>
2018-07-19 23:41:10 +0900

    add repl.go

---
+++ 01/src/monkey/repl/repl.go
@@ -0,0 +1,25 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/kamakuni/writing_an_interpreter_in_go/01/src/monkey/lexer"
+	"github.com/kamakuni/writing_an_interpreter_in_go/01/src/monkey/token"
+	"io"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	fmt.Printf(PROMPT)
+	scanned := scanner.Scan()
+	if !scanned {
+		return
+	}
+	line := scanner.Text()
+	l := lexer.New(line)
+	for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+		fmt.Printf("%+v\n", tok)
+	}
+}
