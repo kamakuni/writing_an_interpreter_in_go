31a7590475eb764e0c029dc7b86efc3740b699f0
kamakuni <superradio86@gmail.com>
2018-08-07 23:38:29 +0900

    add some conditions to TestIdentifierExpression

--- 01/src/monkey/parser/parser_test.go
+++ 01/src/monkey/parser/parser_test.go
@@ -118,4 +118,15 @@ func TestIdentifierExpression(t *testing.T) {
 	if len(program.Statements) != 1 {
 		t.Fatal("program has not enough statements. got=%d", len(program.Statements))
 	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp is not *ast.Identifier, got=%T", stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %s, got=%s", "foobar", ident.Value)
+	}
 }
