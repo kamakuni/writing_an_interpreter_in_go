6ec84a3af738615905a7b320b725a08574855f64
kamakuni <superradio86@gmail.com>
2018-08-25 13:42:56 +0900

    add functions for PrefixExpression

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -31,6 +31,20 @@ type PrefixExpression struct {
 	Right    Expression
 }
 
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+
+}
+
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
