02e8ebf72005158c92c05becc24bb9c0d8c80d83
kamakuni <superradio86@gmail.com>
2018-09-11 22:05:48 +0900

    add methos for InfixExpression

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -151,4 +151,15 @@ type InfixExpression struct {
 	Right    Expression
 }
 
-func (ie *InfixExpression) statementNode()       {}
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
