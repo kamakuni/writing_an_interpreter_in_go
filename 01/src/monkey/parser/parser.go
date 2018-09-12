8a1b030a31425d2b4744800abdf3cfab2c5fd62b
kamakuni <superradio86@gmail.com>
2018-09-12 22:20:22 +0900

    add map for precedences

--- 01/src/monkey/parser/parser.go
+++ 01/src/monkey/parser/parser.go
@@ -25,6 +25,17 @@ type (
 	infixParserFn  func(ast.Expression) ast.Expression
 )
 
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
 type Parser struct {
 	l *lexer.Lexer
 
