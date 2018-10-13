d6780d759c1c48e0ad569076591bd7caf8db5f43
kamakuni <superradio86@gmail.com>
2018-10-13 12:17:14 +0900

    add String method for boolean struct

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -52,6 +52,7 @@ type Boolean struct {
 
 func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
 
 type IntegerLiteral struct {
 	Token token.Token
