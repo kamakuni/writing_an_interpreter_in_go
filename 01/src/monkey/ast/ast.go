f7734a23e7d0ad1a6d970c12bf366bcb2d4785d1
kamakuni <superradio86@gmail.com>
2018-10-13 14:58:26 +0900

    move methods for Program Structure

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -25,6 +25,24 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
@@ -62,24 +80,6 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
-
-func (p *Program) String() string {
-	var out bytes.Buffer
-
-	for _, s := range p.Statements {
-		out.WriteString(s.String())
-	}
-
-	return out.String()
-}
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
-	}
-}
 
 type LetStatement struct {
 	Token token.Token
