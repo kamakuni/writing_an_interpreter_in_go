8723da08ca824f1d77b55c3cf180218d413825d6
kamakuni <superradio86@gmail.com>
2018-10-27 10:47:57 +0900

    add parseGroupedExpression function

--- 01/src/monkey/parser/parser.go
+++ 01/src/monkey/parser/parser.go
@@ -66,6 +66,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPARAN, p.parseGroupedExpression)
 	// Register parser functions for Infix tokens
 	// +, -, /, *, =, != , <, >
 	p.infixParserFns = make(map[token.TokenType]infixParserFn)
@@ -92,6 +93,18 @@ func (p *Parser) parseIdentifier() ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPARAN) {
+		return nil
+	}
+
+	return exp
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
