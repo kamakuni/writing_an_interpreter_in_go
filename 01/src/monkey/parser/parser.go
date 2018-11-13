af7496aa0061aecf6740bb4032b1c46bbbcb5697
kamakuni <superradio86@gmail.com>
2018-11-14 08:25:43 +0900

    add codes to register parsePrefixIfExpression

--- 01/src/monkey/parser/parser.go
+++ 01/src/monkey/parser/parser.go
@@ -67,6 +67,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPARAN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 	// Register parser functions for Infix tokens
 	// +, -, /, *, =, != , <, >
 	p.infixParserFns = make(map[token.TokenType]infixParserFn)
