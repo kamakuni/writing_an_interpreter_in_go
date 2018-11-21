9a3b35fa1a751a73045d8dd377b0fc8f4a15463f
kamakuni <superradio86@gmail.com>
2018-11-21 23:31:37 +0900

    add String method for FunctionLiteral structure

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -2,8 +2,8 @@ package ast
 
 import (
 	"bytes"
-
 	"github.com/kamakuni/writing_an_interpreter_in_go/01/src/monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -27,9 +27,24 @@ type FunctionLiteral struct {
 	Body       *BlockStatement
 }
 
-func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) expressionNode()      {}
 func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
 
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
 
 type Program struct {
 	Statements []Statement
