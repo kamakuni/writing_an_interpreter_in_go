e354b6cd5c2f40bb2e90313f3fa3d950c6353e2b
kamakuni <superradio86@gmail.com>
2018-12-06 22:04:04 +0900

    fix typo

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -194,7 +194,7 @@ type IfExpression struct {
 }
 
 func (ie *IfExpression) expressionNode()      {}
-func (ie *IfExpression) TokenLiteral() string { return ie.TokenLiteral }
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
 func (ie *IfExpression) String() string {
 	var out bytes.Buffer
 
