c67d72290574f371dcc896da370ade7d5fda0bcd
kamakuni <superradio86@gmail.com>
2019-01-02 15:13:47 +0900

    fix a typo

--- 01/src/monkey/object/object.go
+++ 01/src/monkey/object/object.go
@@ -29,5 +29,5 @@ type Boolean struct {
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string {
-	return fmt.Sprintf("%d", b.Value)
+	return fmt.Sprintf("%t", b.Value)
 }
