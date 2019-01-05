dc9dc97f37a19148b46232ca6c7abb042d58a365
kamakuni <superradio86@gmail.com>
2019-01-05 12:05:38 +0900

    add TestEvalIntegerExpression function in evaluator_test.go

--- 01/src/monkey/evaluator/evaluator_test.go
+++ 01/src/monkey/evaluator/evaluator_test.go
@@ -7,6 +7,21 @@ import (
 	"testing"
 )
 
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, expected, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
