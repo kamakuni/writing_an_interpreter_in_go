a4b074820ae683f51b546bcdfca7e870df3e4620
kamakuni <superradio86@gmail.com>
2019-01-07 23:03:04 +0900

    add evalStatements function in evaluator.go

--- 01/src/monkey/evaluator/evaluator.go
+++ 01/src/monkey/evaluator/evaluator.go
@@ -13,3 +13,13 @@ func Eval(node ast.Node) object.Object {
 
 	return nil
 }
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
