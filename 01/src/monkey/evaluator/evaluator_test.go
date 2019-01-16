01d23cf12d1d4931fea6b34bf3036d04da03886c
kamakuni <superradio86@gmail.com>
2019-01-16 22:30:29 +0900

    add TestBangOperator function

--- 01/src/monkey/evaluator/evaluator_test.go
+++ 01/src/monkey/evaluator/evaluator_test.go
@@ -57,6 +57,27 @@ func TestEvalBooleanExpression(t *testing.T) {
 		testBooleanObject(t, evaluted, tt.expected)
 	}
 }
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!ture", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+
+}
+
 func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	result, ok := obj.(*object.Boolean)
 	if !ok {
