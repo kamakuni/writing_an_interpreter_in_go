a3ab37382e50d366c4d4c7e1d9e24132e809ddd7
kamakuni <superradio86@gmail.com>
2019-01-18 23:57:38 +0900

    add evalBangOperatorExpression function

--- 01/src/monkey/evaluator/evaluator.go
+++ 01/src/monkey/evaluator/evaluator.go
@@ -30,7 +30,25 @@ func Eval(node ast.Node) object.Object {
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
-	return NULL
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
