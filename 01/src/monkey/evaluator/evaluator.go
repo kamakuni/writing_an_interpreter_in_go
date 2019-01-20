d65b35ed8cf5b0dcdadfad23089b50a69cfb94fd
kamakuni <superradio86@gmail.com>
2019-01-20 22:40:02 +0900

    add evalMinusPrefixOperatorExpression function

--- 01/src/monkey/evaluator/evaluator.go
+++ 01/src/monkey/evaluator/evaluator.go
@@ -33,6 +33,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -49,6 +51,10 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	return NULL
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
